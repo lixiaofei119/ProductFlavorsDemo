ad6471ef27c2ade481171d20913763c8117a27b9
lixiaofei17 <1195955974@qq.com>
2019-05-05 13:48:16 +0800

    添加项目

--- app/src/main/java/com/lxf/productflavors/MainActivity.kt
+++ app/src/main/java/com/lxf/productflavors/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.lxf.productflavors
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
